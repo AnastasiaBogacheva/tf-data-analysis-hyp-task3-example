2e55eae6d8674179b7008a3a281b9469d50a15c1
AnastasiaBogacheva <130046692+AnastasiaBogacheva@users.noreply.github.com>
2023-04-13 19:41:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ from statsmodels.stats.weightstats import ztest
 chat_id = 694882183 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    if(ztest(x,y)[1]<0.09):
+    if(ztest(x,y)[1]<0.03):
       return True
     else:
       return False
